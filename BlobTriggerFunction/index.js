9f6ff411bbd6fe5d99f7eb317f0d6b4ed16d8185
Thomas Conte <thomas@conte.com>
2016-09-26 13:47:52 +0200

    Jimp implementation

--- BlobTriggerFunction/index.js
+++ BlobTriggerFunction/index.js
@@ -1,10 +1,12 @@
 var Jimp = require("jimp");
 
-module.exports = function (context, inputBlob) {
-    context.log('Node.js blob trigger function processed blob...');
-    Jimp.read(inputBlob, function (err, image) {
+module.exports = function(context, inputBlob) {
+    Jimp.read(inputBlob, function(err, image) {
         image.scale(0.5);
-        context.bindings.outputBlob = image;
-        context.done();
+        image.getBuffer(Jimp.AUTO, function(error, imageData) {
+            context.log('Node.JS blob trigger function resized ' + context.bindingData.name + ' to ' + image.bitmap.width + 'x' + image.bitmap.height);
+            context.bindings.outputBlob = imageData;
+            context.done();
+        });
     });
 };
